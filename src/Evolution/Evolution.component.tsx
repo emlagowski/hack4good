aaa3cbca00e778a6e95b6bbbe141d53d7688d2bf
Marcin Łagowski <marcin.lagowski@nn.pl>
2022-06-03 16:10:27 +0200

    added evolution margin

--- src/Evolution/Evolution.component.tsx
+++ src/Evolution/Evolution.component.tsx
@@ -11,71 +11,32 @@ export interface Point {
   pv: number;
 }
 
-// const data = [
-//   {
-//     name: 'Page A',
-//     uv: 4000,
-//     pv: 2400,
-//   },
-//   {
-//     name: 'Page B',
-//     uv: 3500,
-//     pv: 1398,
-//   },
-//   {
-//     name: 'Page C',
-//     uv: 3000,
-//     pv: 9800,
-//   },
-//   {
-//     name: 'Page D',
-//     uv: 2500,
-//     pv: 3908,
-//   },
-//   {
-//     name: 'Page E',
-//     uv: 2000,
-//     pv: 4800,
-//   },
-//   {
-//     name: 'Page F',
-//     uv: 1500,
-//     pv: 3800,
-//   },
-//   {
-//     name: 'Page G',
-//     uv: 1000,
-//     pv: 4300,
-//   },
-// ];
-
 export const Evolution: React.FC<DataProps> = (props) => {
     const data = props.data;
 
     return (
         <div className={classes.container}>
-            <h2>How much CO<sub>2</sub> do I emmit over time?</h2>
-
-        <ResponsiveContainer height="80%">
-        <LineChart
-          width={500}
-          height={300}
-          data={data}
-          margin={{
-            top: 5,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}
-        >
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
-          <Line type="monotone" dataKey="uv" stroke="#d88e84" dot={false} />
-        </LineChart>
-</ResponsiveContainer>
+          <h2>How much CO<sub>2</sub> do I emmit over time?</h2>
+          <ResponsiveContainer height="80%">
+            <LineChart
+              width={500}
+              height={300}
+              data={data}
+              margin={{
+                top: 40,
+                right: 30,
+                left: 20,
+                bottom: 5,
+              }}
+            >
+              <XAxis dataKey="name" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Line type="monotone" dataKey="pv" name="Your usage" stroke="#8884d8" activeDot={{ r: 8 }} />
+              <Line type="monotone" dataKey="uv" name="Trends" stroke="#d88e84" dot={false} />
+            </LineChart>
+          </ResponsiveContainer>
         </div>
     );
 };
