ec2db1305ca7861802d5cacd6ca000007cfd58d8
Marcin Łagowski <marcin.lagowski@nn.pl>
2022-06-03 16:49:03 +0200

    added mockup

---
+++ src/Mockup/Mockup.component.tsx
@@ -0,0 +1,7 @@
+export const Mockup: React.FC = () => {
+    return (
+        <div style={{width:"100%", height: "100vh"}}>
+            <iframe title="mockup" style={{border: "1px solid rgba(0, 0, 0, 0.1);"}} width="100%" height="100%" src={"https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FGdew6rHLEDpJ983bPSN3lp%2FHackaton%3Fpage-id%3D0%253A1%26node-id%3D23%253A16026%26viewport%3D258%252C349%252C0.74%26scaling%3Dmin-zoom%26starting-point-node-id%3D23%253A16018%26show-proto-sidebar%3D1"} allowFullScreen></iframe>
+        </div>
+    );
+};
